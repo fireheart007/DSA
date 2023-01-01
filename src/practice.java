eec8c9ce6cbccca50d56d40e617e66387bcc87f9
fireheart007 <shivangsharma2000@gmail.com>
2023-01-01 13:47:15 +0530

    initial version

--- src/practice.java
+++ src/practice.java
@@ -3,7 +3,7 @@ import Tree.Leetcode_BinaryTree;
 
 import java.util.*;
 public class practice {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
     }
 }
